5a512d73f5fa4a90a2480ce64e770413a137cfa5
Agustin Goni <agustingoni1710@gmail.com>
2023-07-23 08:38:33 -0300

    create category  item and styles - create themes colors and styles

---
+++ src/components/categories/item/index.jsx
@@ -0,0 +1,15 @@
+import { TouchableHighlight, ImageBackground, Text } from "react-native";
+
+import { styles } from "./styles";
+
+const CategoryItem = ({ id, name, backgroundColor, backgroundImage, onSelectCategory }) => {
+    return(
+        <TouchableHighlight onPress={()=> onSelectCategory(id)} style={[styles.container, {backgroundColor}]}>
+            <ImageBackground source={{ uri: backgroundImage}} style={styles.imageBackground} resizeMode="cover">
+                <Text style={styles.categoryName}>{name}</Text>
+            </ImageBackground>
+        </TouchableHighlight>
+    )
+}
+
+export default CategoryItem;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import { Header } from './components';
-
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { CategoryItem, Header } from './components';
+import CATEGORIES from './constants/data/categories.json';
 export default function App() {
+  const onSelectCategory = ( categoryId ) =>{
+    console.warn({categoryId})
+  }
   return (
     <SafeAreaView style={styles.container}> 
       <View style={styles.container}>
         <Header title='Categories'/>
+        <FlatList
+          data={ CATEGORIES }
+          style={styles.categoryContainer}
+          contentContainerStyle={styles.listCategory}
+          renderItem={({ item }) => <CategoryItem {...item} onSelectCategory={onSelectCategory}/>}
+          keyExtractor={(item) => item.id}
+        />
       </View>
     </SafeAreaView>
   );
@@ -14,7 +24,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    
+    backgroundColor: '#fff',  
   },
+  categoryContainer: {
+    marginTop: 15,
+    marginHorizontal: 15,
+  },
+  listCategory: {
+    gap: 15,
+  }
 });
