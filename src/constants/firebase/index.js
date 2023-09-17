d6e7ddf3301e0680b2f14d6b13ecdc7120472c48
Agustin Goni <agustingoni1710@gmail.com>
2023-09-17 12:00:09 -0300

    [feature] - create stack auth and implement in root navigator

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,2 +1,3 @@
 export const URL_BASE_FIREBASE_REALTIME_DATABASE = 
     'https://mdqcapsstore-default-rtdb.firebaseio.com';
+export const FIREBASE_API_KEY = 'AIzaSyARmL2Id4miriOgiOiEutzuVETCmgeU1PY';
